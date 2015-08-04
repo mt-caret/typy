d4c5255d09b2e349fcf1f2b552de8fd2a3cdc6c2
Masayuki Takeda <mtakeda.enigsol@gmail.com>
2015-08-03 17:37:00 -0700

    remove unneeded dependencies

--- typy.js
+++ typy.js
@@ -4,7 +4,7 @@ function update_str(val) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {str: str}, function(response) {
       if (!response) {
-      chrome.tabs.executeScript(null, {file: "lib/jquery-2.1.4.min.js"});
+      chrome.tabs.executeScript(null, {file: "jquery-2.1.4.min.js"});
       chrome.tabs.executeScript(null, {file: "typer.js"});
       }
     });
